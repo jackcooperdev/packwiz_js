8254982b463f59f49e84754ec5248a9b77f5bd5c
Jack Cooper <dev@jackcooper.me>
2025-01-08 22:06:13 +0000

    :art: Code Tidy up

--- index.js
+++ index.js
@@ -1,10 +1,158 @@
 const fs = require('fs');
 const shell = require('shelljs');
 const {exec} = require('node:child_process');
-let toml = require('toml');
 const path = require('path');
 const {packwizLogger} = require('./logger');
+const { parse } = require('smol-toml')
 const StreamZip = require('node-stream-zip');
+
+// Import From Modrinth (.mrpack)
+
+async function importFromModrinth(mrPath, outPath, packwizLoc, nameOverride) {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if (!packwizLoc) {
+                console.log('no')
+                if (!shell.which('packwiz')) {
+                    reject('No Packwiz exe found! Please Add to Path or declare manually...')
+                    return;
+                } else {
+                    packwizLoc = shell.which('packwiz');
+                }
+            }
+            packwizLoc = path.resolve(packwizLoc.toString());
+            // Check if zip exists
+            if (!fs.existsSync(mrPath)) {
+                reject('FILE_NOT_EXIST');
+            }
+
+            // Check if is ZIP
+            if (path.extname(mrPath) !== '.mrpack') {
+                reject('NOT_VALID_MRPACK');
+            }
+
+            const zip = new StreamZip.async({ file: mrPath });
+            const manifestRaw = await zip.entryData('modrinth.index.json');
+            let manifest = JSON.parse(manifestRaw)
+            let name_over;
+            if (nameOverride) {
+                name_over = nameOverride;
+            } else {
+                name_over = manifest.name;
+            }
+            shell.mkdir('-p', path.join(outPath, name_over));
+            // Create Pack Info
+
+            let projectDeps = manifest.dependencies;
+            let depKeys = Object.keys(projectDeps);
+            let loader = "";
+            let loaderVersion = "";
+            let mcVersion = "";
+
+            for (let idx in depKeys) {
+                if (depKeys[idx] === 'minecraft') {
+                    mcVersion = projectDeps[depKeys[idx]]
+                } else {
+                    loader = depKeys[idx];
+                    loaderVersion = projectDeps[depKeys[idx]];
+                }
+            }
+
+
+            let newPackInfo = {
+                author: 'John Doe',
+                loader: loader,
+                loaderVersion: loaderVersion,
+                minecraftVersion: mcVersion,
+                name: name_over,
+                version: 1
+            }
+
+            //Create Pack
+            await createPack(newPackInfo, path.join(outPath),packwizLoc);
+
+            // Extract Overrides
+            shell.mkdir('-p', path.join(outPath, name_over, 'overrides'));
+            await zip.extract('overrides', path.join(outPath, name_over, 'overrides'));
+            // Sync Index
+            await runPackwiz(packwizLoc,'refresh',path.join(outPath, name_over));
+
+            for (let idx in manifest.files ) {
+                await runPackwiz(packwizLoc,`mr add ${manifest.files[idx].downloads[0]} --yes`,path.join(outPath, name_over));
+            }
+
+            let packInfo = await getPackInfo(path.join(outPath, name_over));
+            resolve(packInfo);
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+
+// Import From CurseForge (.zip)
+async function importFromCurseforge(zipPath, outPath, packwizLoc, nameOverride) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!packwizLoc) {
+                if (!shell.which('packwiz')) {
+                    reject('No Packwiz exe found! Please Add to Path or declare manually')
+                    return;
+                } else {
+                    packwizLoc = shell.which('packwiz');
+                }
+            }
+            packwizLoc = path.resolve(packwizLoc.toString());
+            // Check if zip exists
+
+            if (!fs.existsSync(zipPath)) {
+                reject('FILE_NOT_EXIST');
+            }
+
+            // Check if is ZIP
+            if (path.extname(zipPath) !== '.zip') {
+                reject('NOT_VALID_ZIP');
+            }
+            let name_over;
+            if (nameOverride) {
+                name_over = nameOverride;
+            } else {
+                name_over = path.parse(zipPath).name;
+            }
+            shell.mkdir('-p', path.join(outPath, name_over));
+            let command = `curseforge import ${zipPath}`;
+            await runPackwiz(packwizLoc, command, path.join(outPath, name_over));
+            let packInfo = await getPackInfo(path.join(outPath, name_over));
+            resolve(packInfo);
+
+        } catch (err) {
+            reject(err);
+        }
+    })
+
+
+}
+
+async function getPackInfo(dir) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let packFile = parse(fs.readFileSync(path.join(dir, 'pack.toml')).toString());
+            resolve(packFile);
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
+async function getPackVersion(name, dir) {
+    try {
+        let packFile = parse(fs.readFileSync(path.join(dir, name, 'pack.toml')).toString());
+        return packFile.version;
+    } catch (err) {
+        return false
+    }
+}
 
 async function createPack(fileData, dir, packwizLoc) {
     return new Promise(async (resolve, reject) => {
@@ -20,7 +168,8 @@ async function createPack(fileData, dir, packwizLoc) {
         packwizLoc = path.resolve(packwizLoc.toString());
 
 
-        let command = `init -r --author ${fileData.author} --${fileData.loader}-version ${fileData.loaderVersion} --mc-version ${fileData.minecraftVersion} --modloader ${fileData.loader} --name ${fileData.name} --version ${fileData.version}`;
+        let command = `init -r --author ${fileData.author.split(" ").join("")} --${fileData.loader}-version ${fileData.loaderVersion} --mc-version ${fileData.minecraftVersion} --modloader ${fileData.loader} --name ${fileData.name.split(" ").join("")} --version ${fileData.version}`;
+        console.log(command)
         await runPackwiz(packwizLoc, command, path.join(dir, fileData.name));
         let modArray = fileData.mods;
         for (let idx in modArray) {
@@ -36,10 +185,11 @@ async function getModList(packFolder) {
     return new Promise(async (resolve, reject) => {
         try {
             if (fs.existsSync(packFolder)) {
+                packFolder = path.join(packFolder, 'mods');
                 let folderFiles = fs.readdirSync(packFolder);
                 let mods = [];
                 for (let idx in folderFiles) {
-                    let fileName = toml.parse(fs.readFileSync(path.join(packFolder, folderFiles[idx])).toString()).filename;
+                    let fileName = parse(fs.readFileSync(path.join(packFolder, folderFiles[idx])).toString()).filename;
                     mods.push(fileName);
                 }
                 resolve(mods);
@@ -50,107 +200,18 @@ async function getModList(packFolder) {
     })
 }
 
-async function importFromCurseforge(zipPath, dir, packwizLoc, outputJSON, nameOverride) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!outputJSON) {
-                outputJSON = false;
-            }
 
-            if (!packwizLoc) {
-                if (!shell.which('packwiz')) {
-                    reject('No Packwiz exe found! Please Add to Path or declare manually')
-                    return;
-                } else {
-                    packwizLoc = shell.which('packwiz');
-                }
-            }
-            packwizLoc = path.resolve(packwizLoc.toString());
-            // Check if zip exists
-
-            if (!fs.existsSync(zipPath)) {
-                reject('FILENOTEXIST');
-            }
-
-            if (path.extname(zipPath) !== '.zip') {
-                reject('NOTVALID');
-            }
-
-
-            const manifest = new StreamZip.async({file: zipPath});
-            const manifestDataBuffer = await manifest.entryData('manifest.json');
-            const manifestFile = JSON.parse(manifestDataBuffer.toString());
-            let name_over;
-            if (nameOverride) {
-                name_over = nameOverride;
-            } else {
-                name_over = manifestFile.name
-            }
-
-            let mods = [];
-            let modList = manifestFile.files;
-
-            for (let idx in modList) {
-                let selectedMod = modList[idx]
-                /**
-                 * @param selectedMod.projectID
-                 * @param selectedMod.fileID
-                 */
-                let newObj = {
-                    source: 'cf',
-                    slug: `--addon-id ${selectedMod.projectID} --file-id ${selectedMod.fileID}`
-                };
-                mods.push(newObj);
-            }
-
-            /**
-             * @param manifestFile.minecraft
-             * @param manifestFile.minecraft.modLoaders
-             */
-            let newModPack = {
-                name: name_over,
-                friendly: manifestFile.name,
-                version: 1,
-                minecraftVersion: manifestFile.minecraft.version,
-                loader: manifestFile.minecraft.modLoaders[0].id.split("-")[0],
-                loaderVersion: manifestFile.minecraft.modLoaders[0].id.split("-")[1],
-                mods: mods
-            };
-
-
-            shell.mkdir('-p', path.join(dir, name_over));
-            let command = `curseforge import ${zipPath}`;
-            await runPackwiz(packwizLoc, command, path.join(dir, name_over));
-            if (outputJSON) {
-                resolve(newModPack)
-            } else {
-                resolve(true);
-            }
-        } catch (err) {
-            reject(err);
-        }
-    })
-
-
-}
-
-async function getPackVersion(name, dir) {
-    try {
-        let packFile = toml.parse(fs.readFileSync(path.join(dir, name, 'pack.toml')).toString());
-        return Number(packFile.version);
-    } catch (err) {
-        return false
-    }
-
-}
-
+// Packwiz Runner
 async function runPackwiz(loc, command, dir) {
     return new Promise(async (resolve) => {
+        fs.mkdirSync(dir, {recursive: true});
         let child = exec(`cd ${dir} && ${loc} ${command}`);
         child.stdout.on('data', function (data) {
+            console.log(data)
             packwizLogger.info(data);
         });
         child.stderr.on('data', function (data) {
+            console.log(data)
             packwizLogger.error(data);
         });
         child.on('close', function (code) {
@@ -160,5 +221,4 @@ async function runPackwiz(loc, command, dir) {
     })
 }
 
-
-module.exports = {runPackwiz, createPack, getPackVersion, getModList, importFromCurseforge}
+module.exports = {runPackwiz, importFromCurseforge, getPackVersion, getModList, createPack, importFromModrinth};
